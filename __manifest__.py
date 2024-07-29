17ce2b9c4afaac02c31fadf1a435254c633bfe6a
PacomeKFP <pacomekengafe@gmail.com>
2024-07-29 17:19:35 +0100

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+
+    'summary': "The real estate module as presented in the documentation",
+
+    'description': """
+        This  module covers a business area which is very specific and therefore not included in the standard set of modules: real estate. It is worth noting that before developing a new module, it is good practice to verify that Odoo doesn’t already provide a way to answer the specific business case.
+    """,
+
+    'author': "Pacome Kengali F.",
+    'website': "https://PacomeKFP.github.io/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'license': "LGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'application': True,
+}
---
+++ models/estate_property.py
@@ -0,0 +1,62 @@
+from datetime import timedelta
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Property'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Date Availability', copy=False,
+                                    default=lambda self: fields.Date.today() + timedelta(days=90))
+    active = fields.Boolean(string='Active', default=False)
+    state = fields.Selection(
+        string="State",
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')
+        ],
+        required=True,
+        copy=False,
+        default='new')
+    expected_price = fields.Float(string='Expected Price')
+    selling_price = fields.Float(string='Selling Price', copy=False, readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection([
+        ('north', 'North'),
+        ('south', 'South'),
+        ('east', 'East'),
+        ('west', 'West')
+    ], string='Garden Orientation')
+
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
+
+    _sql_constraints = [
+        ('name_unique', 'unique(name)', 'The property name must be unique.'),
+
+    ]
+
+    # Contraintes
+    """
+    - On ne peut pas preciser d'orientation pour le jardin si  on a pas de jardin
+    - Pas de taille de jardin s'il n'y a pas de jardin 
+    """
+
+    """@api.constrains('expected_price', 'selling_price')
+    def _check_price(self):
+        for record in self:
+            if record.selling_price and record.selling_price > record.expected_price:
+                raise ValidationError('The selling price cannot be higher than the expected price.')
+"""
